8490a39f40262f0ddbc34bb7b5be455c760b846f
Dmitry Kudriavtsev <me@dk0.us>
2018-07-05 23:57:08 -0700

    streamlined ruby code

--- rb/countingsort.rb
+++ rb/countingsort.rb
@@ -1,21 +1,15 @@
 #!/usr/bin/env ruby
-#
+
 def sort(input)
-	output = []
-	counts = Array.new(input.max + 1, 0)
-	input.each do |i|
-		counts[i] += 1
-	end
-	for i in 0..(counts.size - 1)
-		counts[i].times do
-			output << i
-		end
-	end
-	return output
+  output = []
+  counts = Array.new(input.max + 1, 0)
+  input.each do |i|
+    counts[i] += 1
+  end
+  counts.each_with_index do |val, index|
+    val.times { output << index }
+  end
+  output
 end
 
-inp = []
-while i = STDIN.gets
-	inp << i.to_i
-end
-p sort inp
+puts sort(ARGV.map(&:to_i)).join ' '
