da7822ceaa3def3815752f476f594f5ea9c4ef58
Dmitry Kudriavtsev <me@dk0.us>
2018-07-18 21:37:36 -0700

    revised c code

--- c/countingsort.c
+++ c/countingsort.c
@@ -2,33 +2,37 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int max(int* arr, int len) {
-	int out = arr[0];
-	for (int i = 0; i < len; i++)
-		if (arr[i] > out)
-			out = arr[i];
-	return out;
+int max(int* arr, size_t len) {
+	if (len < 1) {
+		return 0;
+	}
+	int greatest = arr[0];
+	for (size_t i = 0; i < len; i++)
+		if (arr[i] > greatest)
+			greatest = arr[i];
+	return greatest;
 }
 
-void sort(int** in, int** out, size_t length) {
-	int* inputs = *in;
-	int* outputs = *out;
-
+void sort(int* inputs, int* outputs, size_t length) {
 	// this is the size of the array of counts
 	int greatest = max(inputs, length);	// find the greatest number in the array
 
 	// allocate the array of counts
 	int* counts = calloc(greatest + 1, sizeof(int));
+	if (counts == NULL) {
+		fprintf(stderr, "Memory allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
 	// count numbers in input array
-	for (int i = 0; i < length; i++) {
+	for (size_t i = 0; i < length; i++) {
 		counts[inputs[i]]++;
 	}
 
-	int counter = 0;	// keep track of where we are in output array
+	size_t counter = 0;	// keep track of where we are in output array
 
 	// loop through all the counts
-	for (int i = 0; i < (greatest + 1); i++) {	// for every count in array
+	for (int i = 0; i <= greatest; i++) {	// for every count in array
 		for (int j = 0; j < counts[i]; j++) {	// loop that many times
 			outputs[counter++] = i;	// add the integer being counted to the output array
 		}
@@ -39,16 +43,27 @@ void sort(int** in, int** out, size_t length) {
 int main(int argc, char** argv) {
 	int *inputs, *outputs;
 	size_t length = argc - 1;	// number of integers to sort
-	inputs = malloc(sizeof(int) * (argc - 1));
-	outputs = malloc(sizeof(int) * (argc - 1));
+
+	if (argc < 2) {
+		fprintf(stderr, "Not enough arguments given.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	inputs = calloc(length, sizeof(int));
+	outputs = calloc(length, sizeof(int));
+
+	if (inputs == NULL || outputs == NULL) {
+		fprintf(stderr, "Memory allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
 	for (int i = 1; i < argc; i++) {
 		inputs[i - 1] = atoi(argv[i]);	// assign arguments to array
 	}
 
-	sort(&inputs, &outputs, length);
+	sort(inputs, outputs, length);
 
-	for (size_t i = 0; i < (length); i++) {
+	for (size_t i = 0; i < length; i++) {
 		printf("%d ", outputs[i]);	// print space separated sorted numbers
 	}
 	printf("\n");
@@ -56,5 +71,5 @@ int main(int argc, char** argv) {
 	free(inputs);
 	free(outputs);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
